5c5d16b0e08ccf688f4f03663ad5377d3e8ec5a8
1molehayo <segsybro@gmail.com>
2020-08-06 00:49:59 +0100

    upgraded react and react-router-dom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PageLayout } from 'layouts/PageContainer';
 
-class Home extends Component {
-  render() {
-    return (
-      <PageLayout pageClass="home">
-        <>
-          <section className="section">
-            <div className="container">
-              <h3 className="heading">Home</h3>
-              <p className="paragraph">This is the home page</p>
-            </div>
-          </section>
-        </>
-      </PageLayout>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <PageLayout pageClass="home">
+      <section className="section">
+        <div className="container">
+          <h3 className="heading">Home</h3>
+          <p className="paragraph">This is the home page</p>
+        </div>
+      </section>
+    </PageLayout>
+  );
+};
 
 export default Home;
--- src/pages/Page.js
+++ src/pages/Page.js
@@ -3,14 +3,12 @@ import { PageLayout } from 'layouts/PageContainer';
 
 const Page = () => (
   <PageLayout pageClass="page">
-    <>
-      <section className="section">
-        <div className="container">
-          <h3 className="heading">Page</h3>
-          <p className="paragraph">This is a generic page</p>
-        </div>
-      </section>
-    </>
+    <section className="section">
+      <div className="container">
+        <h3 className="heading">Page</h3>
+        <p className="paragraph">This is a generic page</p>
+      </div>
+    </section>
   </PageLayout>
 );
 
